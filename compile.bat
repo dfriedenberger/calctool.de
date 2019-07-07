97801b09d548aef310eca9c159e9fe5c9eaf9733
dfriedenberger <git@frittenburger.de>
2019-07-07 10:58:45 +0200

    modified startscripts

--- compile.bat
+++ compile.bat
@@ -1,1 +1,2 @@
-javac -d obj -Xlint -classpath src src/de/calctool/gui/WebServer.java
+#javac -d obj -Xlint -classpath src src/de/calctool/gui/WebServer.java
+/usr/java/jdk1.8.0_111/bin/javac -d obj -Xlint -classpath src:lib/* src/de/calctool/gui/WebServer.java
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-(java -classpath obj de.calctool.gui.WebServer > ./log`date +%d.%m_%H:%M`.txt) >& ./error`date +%d.%m_%H:%M`.txt
+(/usr/java/jdk1.8.0_111/bin/java -classpath "obj:lib/*" de.calctool.gui.WebServer > ./log`date +%d.%m_%H:%M`.txt) >& ./error`date +%d.%m_%H:%M`.txt
 
